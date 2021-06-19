ed4afc07b5c742144f415aaff78f7d964f30b952
Aman Kumar Dewangan <amandewatnitrr@gmail.com>
2021-06-19 10:17:46 +0530

    Add files via upload

--- zenatix edge/threader.py
+++ zenatix edge/threader.py
@@ -2,6 +2,7 @@ import time
 import pyrebase
 import requests
 import random
+import threading, queue
 import decimal
 import csv
 from firebase import firebase
@@ -10,8 +11,9 @@ TOKEN = "ddGzXBrit5jLqp15ssLon8oPUZs1X0IHs3HIHyEZ"  # Put your TOKEN here
 DSN = 'https://bmp180-1569c.firebaseio.com/' # 'https://myapp.firebaseio.com/'
 #SECRET = 'shruti7376036196' # 'secretkey'
 #EMAIL ='shruti456rawal@gmail.com' # 'prateekrai266@gmail.com'
-
+q = queue.Queue()
 #authentication = FirebaseAuthentication(SECRET,EMAIL, True, True)
+buffer = 0
 success = 0
 
 config = {
@@ -26,8 +28,22 @@ count = 0
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-def update_database(count):    
-    buffer = 0
+def update_database(count):  
+  
+    #temp = format(sensor.read_temperature())
+    #pres = format(sensor.read_pressure())
+    #oxi_level = format(95+float(decimal.Decimal(random.randrange(-85,198))/100))
+    #if temp is not None and pres is not None and oxi_level is not None:  
+        #str_temp = ' {0:0.2f} *C '.temp 
+        #str_pres  = ' {0:0.2f} %'.pres
+        #str_alti = ' {0:0.2f} m'.alti
+    #    print('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+    #    print('Pressure = {0:0.2f} KPa'.format(sensor.read_pressure()/1000))
+    #    print('Oxygen Saturation = {0:0.2f} %'.format(float(oxi_level)))
+    #    print('Sealevel Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
+    #    print("[INFO] Attemping to send data")
+    #    print("[INFO] finished")
+    #    print("\n")
     with open('dataset.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
@@ -39,14 +55,36 @@ def update_database(count):
                 from firebase import firebase
                 firebase = firebase.FirebaseApplication('https://bmp180-1569c-default-rtdb.firebaseio.com', None)
                 result = firebase.get('/main_test/1-set/','')
-                #if result['status']>1:
-                    #buffer = buffer + int(result['status'])-1
+                while result['status']>1:
+                    bufferf(row-int(result['status']),result['status'])
                     
             data = {"timestamp":t,"sensor_data":float(k),"sensor":s}
             db.child("main_test").child("1-set").set(data)
             count = count + 1
             success = count- buffer
-            time.sleep(1)
+            time.sleep(3)
     print("Success:",success,", Buffer:",buffer)
     
 update_database(count)
+
+
+
+
+
+
+def bufferf(prow,stat):
+    while stat>1:
+        q.put(prow)
+        stat = stat - 1
+        prow = prow + 1
+        buffer = buffer + 1
+    while q.empty()==False:
+        row = q.get()
+        t = f'{row["Timestamp"]}'
+        k = f'{row["Value"]}'
+        s = f'{row["Sensor"]}'
+        data = {"timestamp":t,"sensor_data":float(k),"sensor":s}
+        db.child("main_test").child("1-set").set(data)
+        q.task_done()
+        time.sleep(5)
+        
